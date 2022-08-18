63d74ed6ffd1af74cee764c770940aae407d279c
Mattyv42 <73085006+MatthewVentura@users.noreply.github.com>
2022-08-18 23:12:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+color = input("Enter a color ")
+Holiday = input("Enter a holiday ")
+Verb = input("Enter a verb ")
+noun = input("Enter a noun ")
+Adjective = input("Enter a Adjective ")
+print()
+print()
+print()
+
+
+print("On a cold snowy " + Holiday + ",")
+print("The sky was such a beautiful shade of " + color + ".")
+print("Me and my family went " + Verb + "for the day!")
+print("Finally, we even " + noun + " at night!")
+print("Overall it was such a " +Adjective+ " day...")
